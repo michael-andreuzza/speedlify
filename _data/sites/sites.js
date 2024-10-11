0b6a3fa5310ff821d45c91392645fcf413f6d677
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-10-11 10:19:58 +0300

    testing

---
+++ _data/sites/sites.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "Unwrapped", // optional, falls back to object key
+	description: "The default sites that get tested",
+	options: {
+		runs: 1,
+		frequency: 1, // (in minutes)
+	},
+	urls: [
+		"https://www.lexingtonthemes.com/",
+		"https://www.colorsanftons.com/",
+		"https://svgdoodles.com/",
+		"https://oxbowui.com/",
+	]
+};
